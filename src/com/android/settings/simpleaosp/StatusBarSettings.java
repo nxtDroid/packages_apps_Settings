63083327a2214979f68ee4c9ca9a294b3568ad05
DvTonder <david.vantonder@gmail.com>
2014-12-18 21:38:32 +0000

    Settings: Add cLock settings to custom settings
    Conflicts:
    	res/values/custom_strings.xml
    	res/xml/custom_settings.xml
    	src/com/android/settings/euphoria/CustomSettings.java
    
    Conflicts:
    	src/com/android/settings/simpleaosp/StatusBarSettings.java
    
    Conflicts:
    	src/com/android/settings/simpleaosp/StatusBarSettings.java
    
    Change-Id: I30577162a34aaa708d60c0bb78b0bb4ffa40bd54

--- src/com/android/settings/simpleaosp/StatusBarSettings.java
+++ src/com/android/settings/simpleaosp/StatusBarSettings.java
@@ -29,10 +29,14 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
     private static final int STATUS_BAR_BATTERY_STYLE_HIDDEN = 4;
     private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 6;
+    private static final String KEY_LOCK_CLOCK = "lock_clock";
+    private static final String KEY_LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
 
     private PreferenceScreen mClockStyle;
     private ListPreference mQuickPulldown;
     private ListPreference mStatusBarBattery;
+    private ListPreference mStatusBarBatteryShowPercent;
+    private PreferenceScreen mLockClock;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,6 +46,14 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         ContentResolver resolver = getActivity().getContentResolver();
 
  	mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+ 	mLockClock = (PreferenceScreen) findPreference(KEY_LOCK_CLOCK);
+        if (!Utils.isPackageInstalled(getActivity(), KEY_LOCK_CLOCK_PACKAGE_NAME)) {
+            prefSet.removePreference(mLockClock);
+        }
+
+ 	mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
+        mStatusBarBatteryShowPercent =
+                (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
 
         int batteryStyle = Settings.System.getInt(
                 resolver, Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0);
